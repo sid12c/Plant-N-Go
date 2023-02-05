82c0203c896abc9202c50fed32e30795def3f523
charles4141233 <77245320+charles4141233@users.noreply.github.com>
2023-02-04 23:07:28 -0500

    Finished Website Prototype

--- Hackathon 2023 Project/js/app.js
+++ Hackathon 2023 Project/js/app.js
@@ -1,7 +1,10 @@
+var http = require('http');
+var fs = require('fs'); // file system library
+var index = fs.readFileSync('../index.html');
+
 // In this file we will have it listen to the serial port 
 // and make it print out to console anything it receives 
 var SerialPort = require("serialport"); // load the serial port library
-
 
 // Creating a parser
 // using the parser will tell node.js how to interpret/receive data coming through the serial port
@@ -16,22 +19,45 @@ const parser = new parsers.Readline({
 // 1.) Go to Arduino Code editor
 // 2.) find something like /dev or com/...
 // 3.) copy and paste it in the parentheses
-var port = new SerialPort('paste here ', {
+var port = new SerialPort('/dev/cu.usbmodem1412301', {
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
     stopBits: 1,
     flowControl: false});
 
+port.on('error', function(err) { console.log('Error: ', err.message); }) // error handling
+
 // attach our port to our parsers object
 port.pipe(parser);
-
-// take the parser and add the ondataevent handler
-// when the parser receives data, it's going to run the function
-parser.on('data', function(data) {
-    console.log(data)
-});
 
 // NOTES:
 // Be sure to close the monitor for the arduino code editor so the serial port is free
 // If you don't, it will throw error message
+
+// creating a server
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(index);
+});
+
+// passing the data from our arduino to the socket
+// initialize socket to app
+var io = require('socket.io').listen(app);
+io.on('connection', function(socket) {
+    console.log("Node.js is listening!");
+})
+ 
+// take the parser and add the ondataevent handler
+// when the parser receives data, it's going to run the function
+parser.on('data', function(data) {
+    
+    setTimeout(() => {
+        console.log(data);
+        console.log("Delayed for 3 seconds.");
+        io.emit('data', data) // emitting the message with the data coming from the serial port
+    }, 3000)
+
+});
+
+app.listen(3000);
